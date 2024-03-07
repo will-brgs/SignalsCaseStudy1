7f734daf0c56dcca0f56cd144c8e89a37a031d27
leela-srinivas <112487951+leela-srinivas@users.noreply.github.com>
2024-03-07 16:33:50 -0600

    naming

--- equalizerFunc.m
+++ equalizerFunc.m
@@ -5,14 +5,14 @@ function audio_output = equalizerFunc (input, Fs, gains, center_band, k_cut)
     R_Hi = zeros(5, 1);
     R_Lo = zeros(5, 1);
     for i = 1:5 % Calculate and allocate A and B values for Hi and Lo Coefficients
-
         % Calc Hipass R
         cutoff_Hi = center_band(i) - (k_cut * center_band(i));
         R_Hi(i) = 1/(2 * pi * C * cutoff_Hi);
-
+        
         % Calc Lopass R
         cutoff_Lo = center_band(i) + (k_cut * center_band(i));
         R_Lo(i) = 1/(2 * pi * C * cutoff_Lo);
+    
     end
     
     %Lowpass Filter Coefficients
@@ -33,21 +33,8 @@ function audio_output = equalizerFunc (input, Fs, gains, center_band, k_cut)
     audio_output = zeros(length(t), 1);
     
     for j = 1:5
-        if i == 1
-        %lsim low
-        input_band = lsim(b_Lo(j,:),a_Lo(j, :), input, t);
-
-        elseif i == 5
-        %Lsim hi
-        input_band = lsim(b_Hi(j,:),a_Hi(j,:), input, t);
-
-        else % ie i = 2:4
-        %lsim low
         input_lo = lsim(b_Lo(j,:),a_Lo(j, :), input, t);
-        %Lsim hi
         input_band = lsim(b_Hi(j,:),a_Hi(j,:), input_lo, t);
-        end
-
         audio_output = audio_output + gains(j)*input_band;
     end      
 end
--- equalizerFunc_v2.m
+++ equalizerFunc_v2.m
@@ -1,18 +1,18 @@
-function audio_output = equalizerFunc_OLD (input, Fs, gains, center_band, k_cut)
+function audio_output = equalizerFunc_v2 (input, Fs, gains, center_band, k_cut)
     t = linspace(0, length(input)/Fs, length(input)); %Sample timepoint vector
     C = 10e-6;
 
     R_Hi = zeros(5, 1);
     R_Lo = zeros(5, 1);
     for i = 1:5 % Calculate and allocate A and B values for Hi and Lo Coefficients
+
         % Calc Hipass R
         cutoff_Hi = center_band(i) - (k_cut * center_band(i));
         R_Hi(i) = 1/(2 * pi * C * cutoff_Hi);
-        
+
         % Calc Lopass R
         cutoff_Lo = center_band(i) + (k_cut * center_band(i));
         R_Lo(i) = 1/(2 * pi * C * cutoff_Lo);
-    
     end
     
     %Lowpass Filter Coefficients
@@ -33,8 +33,21 @@ function audio_output = equalizerFunc_OLD (input, Fs, gains, center_band, k_cut)
     audio_output = zeros(length(t), 1);
     
     for j = 1:5
+        if i == 1
+        %lsim low
+        input_band = lsim(b_Lo(j,:),a_Lo(j, :), input, t);
+
+        elseif i == 5
+        %Lsim hi
+        input_band = lsim(b_Hi(j,:),a_Hi(j,:), input, t);
+
+        else % ie i = 2:4
+        %lsim low
         input_lo = lsim(b_Lo(j,:),a_Lo(j, :), input, t);
+        %Lsim hi
         input_band = lsim(b_Hi(j,:),a_Hi(j,:), input_lo, t);
+        end
+
         audio_output = audio_output + gains(j)*input_band;
     end      
 end
